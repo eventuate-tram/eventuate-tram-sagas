e4a64fa5e3f9870b44e9f9b795e4aaab59477aaf
Chris Richardson <chris@chrisrichardson.net>
2020-02-27 15:14:01 -0800

    Fixed gradle submodule reference

--- _build-and-test-all.sh
+++ _build-and-test-all.sh
@@ -19,7 +19,7 @@ set -e
 if [[ "${SPRING_PROFILES_ACTIVE}" != "ActiveMQ" ]]; then
     ./gradlew :orders-and-customers-spring:cleanTest :orders-and-customers-micronaut:cleanTest build
 else
-    ./gradlew -x orders-and-customers-micronaut-integration-tests:test -x :orders-and-customers-micronaut:test -x :eventuate-tram-sagas-common-micronaut:test :orders-and-customers-spring:cleanTest build
+    ./gradlew -x orders-and-customers-micronaut-integration-tests:test -x :orders-and-customers-micronaut:test -x :eventuate-tram-sagas-micronaut-common:test :orders-and-customers-spring:cleanTest build
 fi
 
 ./gradlew "${database}AllComposeDown"
