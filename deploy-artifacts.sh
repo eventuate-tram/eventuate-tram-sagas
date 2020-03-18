5fece8d4698827f996209604a785728fa71e282e
Chris Richardson <chris@chrisrichardson.net>
2020-03-18 11:31:19 -0700

    publish BUILD-SNAPSHOTs from master

--- deploy-artifacts.sh
+++ deploy-artifacts.sh
@@ -9,6 +9,13 @@ DOCKER_COMPOSE_PREFIX=$(echo ${PWD##*/} | sed -e 's/-//g')_
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
+if [  $BRANCH == "master" ] ; then
+  BUILD_SNAPSHOT=$(sed -e '/^version=/!d' -e 's/^version=\(.*\)-SNAPSHOT$/\1.BUILD-SNAPSHOT/' < gradle.properties)
+  echo master: publishing $BUILD_SNAPSHOT
+  ./gradlew -P version=$BUILD_SNAPSHOT -P deployUrl=${S3_REPO_DEPLOY_URL?} uploadArchives
+  exit 0
+fi
+
 if ! [[  $BRANCH =~ ^[0-9]+ ]] ; then
   echo Not release $BRANCH - no PUSH
   exit 0
