0b695df5a46e60152c1b48ff97b94ffddf837ec6
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2020-07-03 16:16:45 +0300

    Replaced dependencies by compileOnly in starters

--- eventuate-tram-sagas-spring-participant-starter/src/main/java/io/eventuate/tram/sagas/spring/participant/autoconfigure/SpringParticipantAutoConfiguration.java
+++ eventuate-tram-sagas-spring-participant-starter/src/main/java/io/eventuate/tram/sagas/spring/participant/autoconfigure/SpringParticipantAutoConfiguration.java
@@ -2,10 +2,12 @@ package io.eventuate.tram.sagas.spring.participant.autoconfigure;
 
 
 import io.eventuate.tram.sagas.spring.participant.SagaParticipantConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
+@ConditionalOnClass(SagaParticipantConfiguration.class)
 @Import(SagaParticipantConfiguration.class)
 public class SpringParticipantAutoConfiguration {
 }
