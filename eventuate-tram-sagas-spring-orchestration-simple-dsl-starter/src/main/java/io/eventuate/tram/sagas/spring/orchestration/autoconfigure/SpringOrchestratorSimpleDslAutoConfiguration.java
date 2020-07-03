749765cc3a596b722f6963dce2753431c385b4e8
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2020-07-03 19:10:24 +0300

    Replaced compileOnly by compile in starters

--- eventuate-tram-sagas-spring-orchestration-simple-dsl-starter/src/main/java/io/eventuate/tram/sagas/spring/orchestration/autoconfigure/SpringOrchestratorSimpleDslAutoConfiguration.java
+++ eventuate-tram-sagas-spring-orchestration-simple-dsl-starter/src/main/java/io/eventuate/tram/sagas/spring/orchestration/autoconfigure/SpringOrchestratorSimpleDslAutoConfiguration.java
@@ -1,12 +1,10 @@
 package io.eventuate.tram.sagas.spring.orchestration.autoconfigure;
 
 import io.eventuate.tram.sagas.spring.orchestration.SagaOrchestratorConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import(SagaOrchestratorConfiguration.class)
-@ConditionalOnClass(SagaOrchestratorConfiguration.class)
 public class SpringOrchestratorSimpleDslAutoConfiguration {
 }
