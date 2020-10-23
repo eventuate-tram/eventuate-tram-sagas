d487c953a885b610c557f35ec9140e0af33c491c
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2020-10-23 12:04:47 +0300

    Supported docker image publishing using eventuate gradle plugin.

--- deploy-artifacts.sh
+++ deploy-artifacts.sh
@@ -1,43 +1,7 @@
 #! /bin/bash -e
 
+docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
+
 ./gradlew  publishEventuateArtifacts
 
-DOCKER_REPO=eventuateio
-REMOTE_PREFIX=eventuate-tram-sagas
-IMAGES="mysql postgres"
-
-DOCKER_COMPOSE_PREFIX=${PWD##*/}_
-
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
-if ! [[  $BRANCH =~ ^[0-9]+ ]] ; then
-  echo Not release $BRANCH - no PUSH
-  exit 0
-elif [[  $BRANCH =~ RELEASE$ ]] ; then
-  BINTRAY_REPO_TYPE=release
-elif [[  $BRANCH =~ M[0-9]+$ ]] ; then
-    BINTRAY_REPO_TYPE=milestone
-elif [[  $BRANCH =~ RC[0-9]+$ ]] ; then
-    BINTRAY_REPO_TYPE=rc
-else
-  echo cannot figure out bintray for this branch $BRANCH
-  exit -1
-fi
-
-VERSION=$BRANCH
-
-function tagAndPush() {
-  LOCAL=$1
-  REMOTE="$REMOTE_PREFIX-$2"
-  $PREFIX docker tag ${DOCKER_COMPOSE_PREFIX?}$LOCAL $DOCKER_REPO/$REMOTE:$VERSION
-  $PREFIX docker tag ${DOCKER_COMPOSE_PREFIX?}$LOCAL $DOCKER_REPO/$REMOTE:latest
-  echo Pushing $DOCKER_REPO/$REMOTE:$VERSION
-  $PREFIX docker push $DOCKER_REPO/$REMOTE:$VERSION
-  $PREFIX docker push $DOCKER_REPO/$REMOTE:latest
-}
-
-$PREFIX docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
-
-for image in $IMAGES ; do
-    tagAndPush $(echo $image | sed -e 's/-//g')  $image
-done
+./gradlew publishEventuateDockerImages
