ff98a6d70025940ecb5116136c4267b642523bfb
Artem Sidorkin <Artem.Sidorkin@digitalarrowtech.com>
2021-07-26 13:22:28 +0300

    Improved ReactiveSagaCommandDispatcherTest.

--- eventuate-tram-sagas-reactive-participant/src/test/java/io/eventuate/tram/sagas/reactive/participant/ReactiveSagaCommandDispatcherTest.java
+++ eventuate-tram-sagas-reactive-participant/src/test/java/io/eventuate/tram/sagas/reactive/participant/ReactiveSagaCommandDispatcherTest.java
@@ -1,29 +1,52 @@
 package io.eventuate.tram.sagas.reactive.participant;
 
 import io.eventuate.tram.commands.common.Command;
-import io.eventuate.tram.commands.consumer.CommandHandlerParams;
+import io.eventuate.tram.commands.common.CommandMessageHeaders;
 import io.eventuate.tram.commands.consumer.CommandHandlerReplyBuilder;
+import io.eventuate.tram.consumer.common.reactive.ReactiveMessageConsumer;
 import io.eventuate.tram.messaging.common.Message;
+import io.eventuate.tram.messaging.producer.MessageBuilder;
 import io.eventuate.tram.reactive.commands.consumer.ReactiveCommandHandler;
+import io.eventuate.tram.reactive.commands.consumer.ReactiveCommandHandlers;
+import io.eventuate.tram.reactive.messaging.producer.common.ReactiveMessageProducer;
+import io.eventuate.tram.sagas.reactive.common.ReactiveSagaLockManager;
 import org.junit.Test;
 import org.mockito.Mockito;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Collections;
 import java.util.Optional;
 import java.util.function.Function;
 
 public class ReactiveSagaCommandDispatcherTest {
   @Test
   public void testHandlerIsInvokedOnlyOnce() {
-    ReactiveSagaCommandDispatcher reactiveSagaCommandDispatcher =
-            new ReactiveSagaCommandDispatcher(null, null, null, null, null);
-
     Function<String, Message> messageHandlerConsumer = createMessageHandlerConsumer();
 
-    Mono.from(reactiveSagaCommandDispatcher.invoke(createReactiveCommandHandler(messageHandlerConsumer), null, createCommandHandlerParams())).block();
+    ReactiveSagaCommandDispatcher reactiveSagaCommandDispatcher =
+            new ReactiveSagaCommandDispatcher("commandDispatcherId1", createReactiveCommandHandlers(messageHandlerConsumer), mockConsumer(), mockProducer(), mockLockManager());
+
+    Mono.from(reactiveSagaCommandDispatcher.messageHandler(createCommandMessage())).block();
 
     Mockito.verify(messageHandlerConsumer).apply(Mockito.any());
+  }
+
+  private Message createCommandMessage() {
+    return MessageBuilder
+            .withPayload("{}")
+            .withHeader(CommandMessageHeaders.COMMAND_TYPE, TestCommand.class.getName())
+            .withHeader(CommandMessageHeaders.REPLY_TO, "channel2")
+            .withHeader("ID", "id1")
+            .build();
+  }
+
+  private ReactiveCommandHandlers createReactiveCommandHandlers(Function<String, Message> messageHandlerConsumer) {
+    ReactiveCommandHandler reactiveCommandHandler =
+            new ReactiveCommandHandler("channel1", Optional.empty(), TestCommand.class, (message, pathVariables) ->
+                    Flux.just("TEST ITERATION VALUE").map(messageHandlerConsumer));
+
+    return new ReactiveCommandHandlers(Collections.singletonList(reactiveCommandHandler));
   }
 
   private Function<String, Message> createMessageHandlerConsumer() {
@@ -34,12 +57,20 @@ public class ReactiveSagaCommandDispatcherTest {
     return messageHandlerConsumer;
   }
 
-  private CommandHandlerParams createCommandHandlerParams() {
-    return new CommandHandlerParams(CommandHandlerReplyBuilder.withSuccess(), TestCommand.class, Optional.of("/TestResource"));
+  private ReactiveMessageProducer mockProducer() {
+    ReactiveMessageProducer producer =  Mockito.mock(ReactiveMessageProducer.class);
+
+    Mockito.when(producer.send(Mockito.any(), Mockito.any())).thenReturn(Mono.just(CommandHandlerReplyBuilder.withSuccess()));
+
+    return producer;
   }
 
-  private ReactiveCommandHandler createReactiveCommandHandler(Function<String, Message> messageHandlerConsumer) {
-   return new ReactiveCommandHandler(null, null, null, (commandMessage, pathVariables) -> Flux.just("TEST ITERATION VALUE").map(messageHandlerConsumer));
+  private ReactiveSagaLockManager mockLockManager() {
+    return Mockito.mock(ReactiveSagaLockManager.class);
+  }
+
+  private ReactiveMessageConsumer mockConsumer() {
+    return Mockito.mock(ReactiveMessageConsumer.class);
   }
 
   public static class TestCommand implements Command {}
