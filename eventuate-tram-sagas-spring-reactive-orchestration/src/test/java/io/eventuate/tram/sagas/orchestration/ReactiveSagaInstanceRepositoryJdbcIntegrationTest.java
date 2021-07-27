8e1fd3f8758d5f67be01d29cb57b093db165abdb
Artem Sidorkin <Artem.Sidorkin@digitalarrowtech.com>
2021-07-27 19:16:58 +0300

    Improved saga instance repository test.

--- eventuate-tram-sagas-spring-reactive-orchestration/src/test/java/io/eventuate/tram/sagas/orchestration/ReactiveSagaInstanceRepositoryJdbcIntegrationTest.java
+++ eventuate-tram-sagas-spring-reactive-orchestration/src/test/java/io/eventuate/tram/sagas/orchestration/ReactiveSagaInstanceRepositoryJdbcIntegrationTest.java
@@ -16,20 +16,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.singleton;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ReactiveSagaInstanceRepositoryJdbcIntegrationTest.Config.class,  webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @EnableAutoConfiguration
 public class ReactiveSagaInstanceRepositoryJdbcIntegrationTest {
 
-  private String sagaType = UUID.randomUUID().toString();
-  private SagaInstance sagaInstance =
-          new SagaInstance(sagaType, null, "SomeState", "lastRequestId", new SerializedSagaData("sagaDatType", "{}"), Collections.emptySet());
+  private String sagaType = generateId();
+  private String sagaState = generateId();
+  private String lastRequestId = generateId();
+  private String sagaDataType = generateId();
+  private String destination = generateId();
+  private String resource = generateId();
 
   @Configuration
   @Import(EventuateCommonReactiveDatabaseConfiguration.class)
@@ -44,21 +49,64 @@ public class ReactiveSagaInstanceRepositoryJdbcIntegrationTest {
   @Autowired
   private ReactiveSagaInstanceRepository sagaInstanceRepository;
 
+  private SagaInstance sagaInstance;
+
   @Test
   public void shouldSaveLoadUpdate() {
-    assertNotNull(sagaInstance.getStateName());
+    sagaInstance = createSagaInstance();
     sagaInstanceRepository.save(sagaInstance).block();
-    SagaInstance result = sagaInstanceRepository.find(sagaType, sagaInstance.getId()).block();
-    assertNotNull(result);
-    assertEquals(sagaInstance.getStateName(), result.getStateName());
+    sagaInstance = sagaInstanceRepository.find(sagaType, sagaInstance.getId()).block();
+    assertSavedSagaInstance();
+    updateSagaInstance();
+    assertUpdatedSagaInstance();
+  }
+
+  private void assertSavedSagaInstance() {
+    assertEquals(sagaState, sagaInstance.getStateName());
+    assertEquals(lastRequestId, sagaInstance.getLastRequestId());
+    assertEquals(sagaDataType, sagaInstance.getSerializedSagaData().getSagaDataType());
+    assertEquals("{}", sagaInstance.getSerializedSagaData().getSagaDataJSON());
+    Set<DestinationAndResource> destinationAndResources = sagaInstance.getDestinationsAndResources();
+    assertEquals(singleton(new DestinationAndResource(destination, resource)), destinationAndResources);
+    DestinationAndResource destinationAndResource = destinationAndResources.stream().findAny().get();
+    assertEquals(destination, destinationAndResource.getDestination());
+    assertEquals(resource, destinationAndResource.getResource());
+  }
+
+  private void updateSagaInstance() {
     sagaInstance.setStateName("UpdatedState");
+    sagaInstance.setLastRequestId("UpdateLastId");
+    sagaInstance.setSerializedSagaData(new SerializedSagaData("UpdatedSagaType", "{\"value\" : \"updatedValue\"}"));
+    sagaInstance.getDestinationsAndResources().add(new DestinationAndResource("newDestination", "newResource"));
     sagaInstanceRepository.update(sagaInstance).block();
-    result = sagaInstanceRepository.find(sagaType, sagaInstance.getId()).block();
-    assertEquals("UpdatedState", result.getStateName());
+    sagaInstance = sagaInstanceRepository.find(sagaType, sagaInstance.getId()).block();
+  }
+
+  private void assertUpdatedSagaInstance() {
+    assertEquals("UpdatedState", sagaInstance.getStateName());
+    assertEquals("UpdateLastId", sagaInstance.getLastRequestId());
+    assertEquals("UpdatedSagaType", sagaInstance.getSerializedSagaData().getSagaDataType());
+    assertEquals("{\"value\" : \"updatedValue\"}", sagaInstance.getSerializedSagaData().getSagaDataJSON());
+
+    Set<DestinationAndResource> destinationAndResources = sagaInstance.getDestinationsAndResources();
+    assertEquals(new HashSet<>(asList(new DestinationAndResource(destination, resource), new DestinationAndResource("newDestination", "newResource"))), destinationAndResources);
   }
 
   @Test(expected = RuntimeException.class)
   public void shouldThrowExceptionWhenInstanceNotFound() {
     sagaInstanceRepository.find(sagaType, "unknown").block();
+  }
+
+  private SagaInstance createSagaInstance() {
+    return new SagaInstance(sagaType,
+            null,
+            sagaState,
+            lastRequestId,
+            new SerializedSagaData(sagaDataType, "{}"),
+            singleton(new DestinationAndResource(destination, resource)));
+  }
+
+  private String generateId() {
+    return UUID.randomUUID().toString();
   }
 }
