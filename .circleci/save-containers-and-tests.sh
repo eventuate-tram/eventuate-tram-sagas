87a9a135c1771c4686558efad3dc2d2d0b16b50e
Chris Richardson <chris@chrisrichardson.net>
2022-04-07 14:38:42 +0900

    Fixes for CircleCI

--- .circleci/save-containers-and-tests.sh
+++ .circleci/save-containers-and-tests.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -e
 
-mkdir -p ~/junit ~/container-logs
-docker ps -a > ~/container-logs/containers.txt
+mkdir -p ~/junit /home/circleci/container-logs
+docker ps -a > /home/circleci/container-logs/containers.txt
 find . -type f -regex ".*/build/test-results/.*xml" -exec cp {} ~/junit/ \;
-sudo bash -c 'find /var/lib/docker/containers -name "*-json.log" -exec cp {} ~/container-logs \;'
-sudo bash -c 'find  ~/container-logs -type f -exec chown circleci {} \;'
+sudo bash -c 'find /var/lib/docker/containers -name "*-json.log" -exec cp {} /home/circleci/container-logs \;'
+sudo bash -c 'find  /home/circleci/container-logs -type f -exec chown circleci {} \;'
