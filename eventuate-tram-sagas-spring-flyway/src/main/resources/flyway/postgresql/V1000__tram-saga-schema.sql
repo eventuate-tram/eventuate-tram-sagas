a2eed4e3fa12e5fa5f30051574c5eb4b3d127867
Chris Richardson <chris@chrisrichardson.net>
2022-12-15 21:33:27 -0800

    #91 Use flyway to configure eventuate DB schema

---
+++ eventuate-tram-sagas-spring-flyway/src/main/resources/flyway/postgresql/V1000__tram-saga-schema.sql
@@ -0,0 +1,35 @@
+CREATE TABLE saga_instance_participants (
+                                                      saga_type VARCHAR(255) NOT NULL,
+                                                      saga_id VARCHAR(100) NOT NULL,
+                                                      destination VARCHAR(100) NOT NULL,
+                                                      resource VARCHAR(100) NOT NULL,
+                                                      PRIMARY KEY(saga_type, saga_id, destination, resource)
+);
+
+CREATE TABLE saga_instance(
+                                        saga_type VARCHAR(255) NOT NULL,
+                                        saga_id VARCHAR(100) NOT NULL,
+                                        state_name VARCHAR(100) NOT NULL,
+                                        last_request_id VARCHAR(100),
+                                        end_state BOOLEAN,
+                                        compensating BOOLEAN,
+                                        failed BOOLEAN,
+                                        saga_data_type VARCHAR(1000) NOT NULL,
+                                        saga_data_json VARCHAR(1000) NOT NULL,
+                                        PRIMARY KEY(saga_type, saga_id)
+);
+
+create table saga_lock_table(
+                                          target VARCHAR(100) PRIMARY KEY,
+                                          saga_type VARCHAR(255) NOT NULL,
+                                          saga_Id VARCHAR(100) NOT NULL
+);
+
+create table saga_stash_table(
+                                           message_id VARCHAR(100) PRIMARY KEY,
+                                           target VARCHAR(100) NOT NULL,
+                                           saga_type VARCHAR(255) NOT NULL,
+                                           saga_id VARCHAR(100) NOT NULL,
+                                           message_headers VARCHAR(1000) NOT NULL,
+                                           message_payload VARCHAR(1000) NOT NULL
+);
