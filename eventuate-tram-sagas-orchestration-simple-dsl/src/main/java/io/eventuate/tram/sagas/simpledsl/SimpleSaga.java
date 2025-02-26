832811cdba19e7f7e5ff1923910478eeeaa67635
Chris Richardson <chris@chrisrichardson.net>
2025-02-26 15:40:26 +0900

    #105 Implement SimpleSaga.getParticipantProxies() methods that allows saga to return the list of the proxy objects for the saga participants

--- eventuate-tram-sagas-orchestration-simple-dsl/src/main/java/io/eventuate/tram/sagas/simpledsl/SimpleSaga.java
+++ eventuate-tram-sagas-orchestration-simple-dsl/src/main/java/io/eventuate/tram/sagas/simpledsl/SimpleSaga.java
@@ -2,5 +2,9 @@ package io.eventuate.tram.sagas.simpledsl;
 
 import io.eventuate.tram.sagas.orchestration.Saga;
 
+import java.util.Collections;
+import java.util.List;
+
 public interface SimpleSaga<Data> extends Saga<Data>, SimpleSagaDsl<Data> {
+  default List<Object> getParticipantProxies() { return Collections.emptyList(); }
 }
