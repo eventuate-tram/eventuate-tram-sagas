10645702ea0a46551cd744d0f871ef37e1df15d4
Chris Richardson <chris@chrisrichardson.net>
2025-08-13 08:37:53 +0900

    Removed attempt to build micronaut projects

--- _build-and-test-all.sh
+++ _build-and-test-all.sh
@@ -13,7 +13,8 @@ if [ "${database}" == "mysql" ]; then
 fi
 
 if [[ "${SPRING_PROFILES_ACTIVE}" != "ActiveMQ" ]]; then
-    ./gradlew :orders-and-customers-spring:cleanTest :orders-and-customers-micronaut:cleanTest build
+    # ./gradlew :orders-and-customers-spring:cleanTest :orders-and-customers-micronaut:cleanTest build
+    ./gradlew build
 
     export EVENTUATE_OUTBOX_ID=1
     export USE_DB_ID=true
@@ -21,9 +22,10 @@ if [[ "${SPRING_PROFILES_ACTIVE}" != "ActiveMQ" ]]; then
     ./gradlew "${database}${broker}AllComposeDown"
     ./gradlew "${database}${broker}AllComposeUp"
 
-    ./gradlew :orders-and-customers-spring:cleanTest :orders-and-customers-micronaut:cleanTest build
-else
-    ./gradlew -x orders-and-customers-micronaut-integration-tests:test -x :orders-and-customers-micronaut:test -x :eventuate-tram-sagas-micronaut-common:test :orders-and-customers-spring:cleanTest build
+    ./gradlew :orders-and-customers-spring:cleanTest build
+ else
+  # ./gradlew -x orders-and-customers-micronaut-integration-tests:test -x :orders-and-customers-micronaut:test -x :eventuate-tram-sagas-micronaut-common:test :orders-and-customers-spring:cleanTest build
+  ./gradlew :orders-and-customers-spring:cleanTest build
 fi
 
 ./gradlew "${database}${broker}AllComposeDown"
