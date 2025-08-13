7387254ca79f4603d791ac546af5eb6ea8d56193
Chris Richardson <chris@chrisrichardson.net>
2025-08-13 09:59:19 +0900

    Start registry

--- mysql/build-docker-multi-arch.sh
+++ mysql/build-docker-multi-arch.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$(cd $( dirname "${BASH_SOURCE[0]}" ) ; pwd)
 
-# docker compose -f $SCRIPT_DIR/../docker-compose-registry.yml --project-name eventuate-common-registry up -d registry
+docker compose -f $SCRIPT_DIR/../docker-compose-registry.yml --project-name eventuate-common-registry up -d registry
 
 IMAGE="${MYSQL_MULTI_ARCH_IMAGE:-${DOCKER_HOST_NAME:-host.docker.internal}:5002/eventuate-tram-sagas-mysql:multi-arch-local-build}"
 OPTS="${BUILDX_PUSH_OPTIONS:---output=type=image,push=true,registry.insecure=true}"
