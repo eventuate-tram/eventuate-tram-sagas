5966c520e4976b6ce8340d0aa2546b1a06ffcb7c
Chris Richardson <chris@chrisrichardson.net>
2025-08-13 10:30:17 +0900

    Hack to deal with conflicting definitions of the Orders table.

--- _build-and-test-all.sh
+++ _build-and-test-all.sh
@@ -8,13 +8,17 @@ export broker="${broker:-kafka}"
 ./gradlew "${database}${broker}AllComposeDown"
 ./gradlew "${database}${broker}AllComposeUp"
 
-if [ "${database}" == "mysql" ]; then
-  cat schema-for-testing-reactive-framework.sql | ./mysql-cli.sh -i
-fi
 
 if [[ "${SPRING_PROFILES_ACTIVE}" != "ActiveMQ" ]]; then
     # ./gradlew :orders-and-customers-spring:cleanTest :orders-and-customers-micronaut:cleanTest build
-    ./gradlew build
+    ./gradlew -x :orders-and-customers-spring-reactive:test build
+
+    if [ "${database}" == "mysql" ]; then
+      ./gradlew "${database}${broker}AllComposeDown"
+      ./gradlew "${database}${broker}AllComposeUp"
+      cat schema-for-testing-reactive-framework.sql | ./mysql-cli.sh -i
+      ./gradlew :orders-and-customers-spring-reactive:test
+    fi
 
     export EVENTUATE_OUTBOX_ID=1
     export USE_DB_ID=true
@@ -22,10 +26,10 @@ if [[ "${SPRING_PROFILES_ACTIVE}" != "ActiveMQ" ]]; then
     ./gradlew "${database}${broker}AllComposeDown"
     ./gradlew "${database}${broker}AllComposeUp"
 
-    ./gradlew :orders-and-customers-spring:cleanTest build
+    ./gradlew :orders-and-customers-spring:cleanTest -x :orders-and-customers-spring-reactive:test build
  else
   # ./gradlew -x orders-and-customers-micronaut-integration-tests:test -x :orders-and-customers-micronaut:test -x :eventuate-tram-sagas-micronaut-common:test :orders-and-customers-spring:cleanTest build
-  ./gradlew :orders-and-customers-spring:cleanTest build
+  ./gradlew :orders-and-customers-spring:cleanTest -x :orders-and-customers-spring-reactive:test build
 fi
 
 ./gradlew "${database}${broker}AllComposeDown"
